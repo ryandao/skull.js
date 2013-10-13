f3efb67913dda474a52a7b7cb40f684bc8e4e8ea
Ryan Dao <daoduyducduong@gmail.com>
2013-10-13 18:06:36 +0800

    Object inheritance API

--- src/system/core.js
+++ src/system/core.js
@@ -27,42 +27,31 @@ Skull.Object = Class.create({
 });
 
 /**
-  Use Prototype API to define the set of custom methods
-  Skull.Object will have. All the subclasses of Skull.Object
-  will inherit the methods defined here.
+  Return a collection of class methods that are available for all classes in Skull.
+  It contains core methods like extend() and create() which are copied to
+  the constructors created using extend() itself.
 */
-Object.extend(Skull, {
-  /**
-    Create a new subclass of this current class.
-  */
-  extend: function() {
-    return Class.create(this, arguments);
-  },
+var classMethods = function() {
+  return {
+    extend: function() {
+      var subclass = Class.create(this, arguments[0]);
+      Object.extend(subclass, classMethods());
 
-  /**
-    Create a new instance of the current class
-  */
-  create: function() {
+      return subclass;
+    },
 
-  },
+    create: function() {
+      var instance = new this();
+      if (arguments.length > 0 && typeof arguments === 'object') {
+        Object.extend(instance, arguments[0]);
+      }
 
-  /**
-    A function that has will be called when a new instance is created.
-    It is useful for custom setup for the instance. For example:
-
-      var MyClass = Skull.Object.extend({
-        initialize: function() {
-          this.test = "test";
-        }
-      })
-  */
-  initialize: function() {
-
+      return instance;
+    }
   }
-})
+};
+
 /**
-  Alias for Protottype class extension
+  Add the class methods to Skull.Object.
 */
-Skull.extend = function() {
-  return Class.create(this, arguments);
-}
+Object.extend(Skull.Object, classMethods());
