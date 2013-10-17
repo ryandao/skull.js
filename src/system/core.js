0247eba17ac52f3741a19a5b45b4c7980553ea01
Ryan Dao <daoduyducduong@gmail.com>
2013-10-17 17:21:18 +0800

    Add event mixin

--- src/system/core.js
+++ src/system/core.js
@@ -92,9 +92,50 @@ Skull.ClassMixin = (function() {
 })();
 
 /**
+  A mixin that can be added to an object to enable event functionality.
+  This will be mixed into the root Skull.Event object, so most of the
+  time you don't need to call this module specifically.
+*/
+Skull.Events = {
+  addListener: function(name, callback) {
+    if (! callback) { return this; }
+    this._listeners || (this._listeners = {});
+    var listeners = this._listeners[name] || (this._listeners[name] = []);
+    listeners.push(callback);
+    return this;
+  },
+
+  removeListener: function(name, callback) {
+    var listeners, retain;
+
+    if (! this._listeners) { return this; }
+    if (listeners = this._listeners[name]) {
+      this._listeners = retain = [];
+      for (var i = 0; i < listeners.length; i++) {
+        if (this._listeners[i] !== callback) {
+          retain.push(this._listeners[i]);
+        }
+      }
+    }
+    return this;
+  },
+
+  sendEvent: function(eventName, params) {
+    if (! this._listeners) { return this; }
+    var listeners = this._listeners[eventName];
+    for (var i = 0; i < listeners.length; i++) {
+      var callback = listeners[i];
+      // TODO: Set the target for the event
+      callback.apply(null, params);
+    }
+    return this;
+  }
+}
+
+/**
   The core object for all objects to inherit from
 */
-Skull.Object = Class.create({
+Skull.Object = Class.create(Skull.Events, {
   /**
     All objects should use this method for getting their attributes.
     So instead of `person.name`, use `person.get('name')`.
