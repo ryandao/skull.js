13c67e6c88fd1e16bb48df17d69c5280a97ef169
Ryan Dao <daoduyducduong@gmail.com>
2013-10-21 00:09:00 +0800

    Add View Class

--- src/view/view.js
+++ src/view/view.js
@@ -1,8 +1,51 @@
 Skull.View = Skull.Object.extend({
-  templateName: Skull.P,
+  templateId: '',
+
+  reRender: function() {
+    this.render();
+  }.observes('all'),
 
   initialize: function() {
-    var source = $(this.templateName).html();
+    this.__scriptEl__ = $('#' + this.get('templateId'));
+    this.render();
+    return this;
+  },
+
+  render: function() {
+    var source = this.__scriptEl__.html();
     var template = Handlebars.compile(source);
+    var html = template({
+      view: this,
+      controller: this.get('controller')
+    });
+    var el = $(document.createElement('div')).append(html);
+
+    if (this.$) {
+      this.$.replaceWith(el);
+    } else {
+      this.__scriptEl__.replaceWith(el);
+    }
+
+    this.$ = el;
+    this.setupHandler();
+
+    return this;
+  },
+
+  /**
+    Setup the action handlers for the view's element.
+    Currently only click event is supported. Other
+    events will be added later.
+  */
+  setupHandler: function(event, eventName) {
+    this.$.on('click', '[data-skull-action]', function(event) {
+      var actionId = $(event.currentTarget).attr('data-skull-action');
+      var action = Skull.ActionHelper.actions[actionId];
+      var handler = action.handler;
+
+      if (action.eventName === 'click') {
+        handler(event);
+      }
+    });
   }
 });
