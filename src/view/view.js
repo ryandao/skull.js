5137f5babcb93c92f131dd2f217a5fb3c6b61dd0
Ryan Dao <daoduyducduong@gmail.com>
2013-10-22 11:12:40 +0800

    Set up the bindings in view

--- src/view/view.js
+++ src/view/view.js
@@ -1,9 +1,6 @@
 Skull.View = Skull.Object.extend({
   templateId: '',
-
-  reRender: function() {
-    this.render();
-  }.observes('all'),
+  rootEl: 'body',
 
   initialize: function() {
     this.__scriptEl__ = $('#' + this.get('templateId'));
@@ -27,25 +24,71 @@ Skull.View = Skull.Object.extend({
     }
 
     this.$ = el;
-    this.setupHandler();
+    this.setupHandlers();
+    this.setupBindings();
 
     return this;
   },
 
   /**
     Setup the action handlers for the view's element.
-    Currently only click event is supported. Other
-    events will be added later.
+    It basically proxies the DOM events to the actions
+    set up by the view and Handlebars template.
   */
-  setupHandler: function(event, eventName) {
-    this.$.on('click', '[data-skull-action]', function(event) {
+  setupHandlers: function() {
+    var eventNames = [
+      'keydown', 'keyup', 'keypress', 'mousedown', 'mouseup', 'click',
+      'doubleclick', 'mousemove', 'focusin', 'focusout', 'mouseenter',
+      'mouseleave', 'submit', 'input', 'change'
+    ]
+
+    for (var i = 0; i < eventNames.length; i++) {
+      this.setupHandler(eventNames[i]);
+    }
+  },
+
+  setupHandler: function(eventName) {
+    this.$.on(eventName, '[data-skull-action]', function(event) {
       var actionId = $(event.currentTarget).attr('data-skull-action');
       var action = Skull.ActionHelper.actions[actionId];
       var handler = action.handler;
 
-      if (action.eventName === 'click') {
+      if (action.eventName === eventName) {
         handler(event);
       }
     });
-  }
+  },
+
+  /**
+    Set up the data binding specify using the {{bind}} helper.
+    The bound attribute needs be belong to either the view or the controller.
+  */
+  setupBindings: function() {
+    var view = this;
+    var controller = view.controller;
+
+    $('[data-skull-binding]', this.$).each(function() {
+      var el = $(this),
+          bindingId = el.attr('data-skull-binding'),
+          propertyPath = Skull.BindingHelper.bindings[bindingId].path,
+          parts = propertyPath.split('.'),
+          root = parts[0],
+          propertyName = parts[1];
+
+      switch(root) {
+        case 'view':
+          root = view;
+          break;
+        case 'controller':
+          root = controller;
+          break;
+        default:
+          throw new Error("Path root of template binding has to be either view or controller");
+      }
+
+      root.addObserver(propertyName, function() {
+        el.html(this.get(propertyName));
+      });
+    });
+  },
 });
