d0d9d6c0a27ce2a09693b005ccbd73f546afa694
Ryan Dao <daoduyducduong@gmail.com>
2013-11-11 03:48:23 +0800

    Default initialization for route class

--- src/router/route.js
+++ src/router/route.js
@@ -1,3 +1,15 @@
 Skull.Route = Skull.Object.extend({
+  setup: Skull.P,
 
+  initialize: function() {
+    if (typeof this.controllerClass !== 'undefined') {
+      this.controller = this.controllerClass.create();
+    }
+
+    if (typeof this.viewClass !== 'undefined') {
+      this.view = this.viewClass.create({ controller: this.controller });
+    }
+
+    return this;
+  }
 });
