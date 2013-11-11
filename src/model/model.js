2cd6cc106f2f595935d5e018cc74a25ace170c88
Ryan Dao <daoduyducduong@gmail.com>
2013-11-11 17:59:43 +0800

    Add Record class.

--- src/model/model.js
+++ src/model/model.js
@@ -1,5 +1,33 @@
 Skull.Model = Skull.Object.extend({
   url: Skull.P,
+  recordUrl: Skull.P
+});
+
+/**
+ * A record encapsulates the JSON response and provides
+ * async retrieval of the attributes.
+ * TODO: Allow binding async record with the view.
+ */
+Skull.Record = Skull.Object.extend({
+  isLoaded: false,
+  recordDidLoad: Skull.P,
+
+  initialize: function() {
+    var record = this;
+
+    if (typeof this.promise !== 'undefined') {
+      this.promise.done(function(data) {
+        record.set('content', data);
+        record.set('isLoaded', true);
+        record.recordDidLoad();
+      });
+    }
+  },
+
+  // Proxy the getter to `content`
+  get: function(attr) {
+    return this.content.get('attr');
+  }
 });
 
 Skull.RecordArray = Skull.ArrayProxy.extend({
